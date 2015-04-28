4a5e1ef13f2997a643ed34499722f2c520768256
hanappe <hanappe@csl.sony.fr>
2015-04-28 23:13:09 +0200

    fixed spelling err

--- public/CitizenSeedsApp.js
+++ public/CitizenSeedsApp.js
@@ -737,7 +737,7 @@ function ObservationMatrixView(matrix, viewStart, numWeeks)
 
         this.button = new Button("createObserver_" + this.matrix.plant.id,
                                  "",
-                                 "Rajouter un ligne pour mes photos.",
+                                 "Rajouter une ligne pour mes photos.",
                                  "createObserver");
         this.button.addListener(_controller);
         this.button.matrix = this.matrix;
